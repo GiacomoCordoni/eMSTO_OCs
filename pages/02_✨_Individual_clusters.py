7649c8e1ae794c8af79669b5e12cf4e6a1414d5b
GiacomoCordoni <gcordoni94@gmail.com>
2024-06-23 16:15:26 +1000

    Working CMD

--- pages/02_✨_Individual_clusters.py
+++ pages/02_✨_Individual_clusters.py
@@ -90,7 +90,7 @@ fig_cmd.update_layout(xaxis_title='G<sub>BP</sub> - G<sub>BP</sub>',
 
 df['vbtoff'] = (df['in_toff']) & (df['vbok'])
 fig4 = px.scatter(df.loc[df['vbtoff']], x='dcn', y='vbroad', hover_data='source_id',
-        opacity=0.99, color= 'w_vbroad', color_continuous_scale = 'greens', trendline='ols', 
+        opacity=0.99, color= 'w_vbroad', color_continuous_scale = 'greens', 
         error_y='vbroad_error')
 
 fig4.update_traces(marker=dict(size=12,
